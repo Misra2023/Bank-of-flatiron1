b0e6708fa07484b94213188e4aafa03c3a9d08e1
Misra2023 <Mishazabdi@gmail.com>
2023-07-27 12:32:53 +0300

    completed assignment

--- misra-app/src/App.js
+++ misra-app/src/App.js
@@ -1,45 +1,68 @@
-import React, { useEffect, useState } from "react";
-import AccountContainer from "./components/AccountContainer";
+import React, { useEffect, useState } from "react"
+
 function App() {
-  const [transaction, setTransaction] = useState([]);
-  // const [search, setSearch] = useState("");
+  const [transactions, setTransactions] = useState([]);
+  const [search, setSearch] = useState("");
 
-
-  useEffect(() => {       
+  useEffect(() => {
     fetch("https://codechallengeone.vercel.app/transactions")
       .then((res) => res.json())
-      .then((data) => setTransaction(data));
+      .then((data) => setTransactions(data));
   }, []);
 
-
-  const handleSearching = (Search)=>{
-    let newOutput = transaction.filter((newTransaction)=>{
-    return newTransaction.description.toLowerCase().includes(Search.toLowerCase())
-     
- 
-    })
-    setTransaction(newOutput)
- }
-
-  const handleTransList = (addTrans) => {
-    const newTransList = [...transaction, addTrans];
-    setTransaction(newTransList);
+  const handleSearching = (searchValue) => {
+    setSearch(searchValue);
   };
+
+  
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
         <h2>The Royal Bank of Flatiron</h2>
       </div>
-      <AccountContainer
-        getTransactions={handleTransList}
-        transaction={transaction}
-        handleSearching = {handleSearching}
-        
-      />
+      {/* Render Search Input */}
+      <div className="ui large fluid icon input">
+        <input
+          type="text"
+          placeholder="Search your Recent Transactions"
+          onChange={(e) => {
+            handleSearching(e.target.value);
+          }}
+        />
+        <i
+          className="circular search link icon"
+          onClick={() => {
+            handleSearching(search);
+          }}
+        ></i>
+      </div>
+
+      {/* Add Transaction Form */}
+      {/* Implement your form here */}
+
+      {/* Render TransactionList */}
+      <TransactionList transactions={filteredTransactions} />
     </div>
   );
 }
 
-export default App;
+function TransactionList({ transactions }) {
+  const trans = transactions.map((transObj, index) => {
+    return (
+      <tr key={index}>
+        <td>{transObj.date}</td>
+        <td>{transObj.category}</td>
+        <td>{transObj.description}</td>
+        <td>{transObj.amount}</td>
+      </tr>
+    );
+  });
+  return <tbody>{trans}</tbody>;
+}
 
+export default App;
